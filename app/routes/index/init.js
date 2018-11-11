431940a9e69e2e9d7a4d2f2b2d70043988e8fed7
kifran <cr91700kifran@gmail.com>
2018-11-11 15:36:21 +0100

    test

--- app/routes/index/init.js
+++ app/routes/index/init.js
@@ -23,7 +23,7 @@ function initIndex(app, session, client, moment, arriverGDL, departMelun, heured
       }).catch((error) => {
         console.log(error);
       });
-    };
+    }; 
 
     getBestJourney = () => {
       client.get('coord/' + position + '/places_nearby?distance=20000&type%5B%5D=stop_point&start_page=1&count=100&').then(function (result) {
